2f784762363efc58a6d7742bc1e29c30dc277e5a
SKM <sanatankrmishra@gmail.com>
2025-03-31 14:44:05 -0700

    Journey started of Day 0

---
+++ homework8_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row, column;
+
+    for(row=1; row<=5; row=row+1)
+    {
+        for(column=1; column<=6; column=column+1)
+        {
+            cout << "4"<<" ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
